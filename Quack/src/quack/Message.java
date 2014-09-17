0325961a0217f696674a1a7d44f5ab6e71ebb5e0
augustomorgan <augustosantosmorgan@gmail.com>
2014-09-17 20:19:48 -0300

    Interface de Mensagem atualizada

--- Quack/src/quack/Message.java
+++ Quack/src/quack/Message.java
@@ -3,22 +3,49 @@ package quack;
 import java.util.Calendar;
 import java.util.List;
 
-
-
 public interface Message {
 	
-	boolean postMessage(String userId, String message);
+	//Basic method to post a single message
+	public boolean postMessage(String userId, String message);
 	
-	//boolean postMessage(String userId, String message, String imageLink);
+	//Post a message with the place information
+	public boolean postMessage(String userId, String message, String place);
 	
-	boolean postReMessage(String userId, String messageId);
+	//Post a message with an image
+	public boolean postMessage(String userId, String message, Long imageId);
 	
-	List<MessageImpl> getUserMessages(String userId);
+	//Post a message with place information and an image
+	public boolean postMessage(String userId, String message, String place, Long imageId);
 	
-	List<MessageImpl> getFollowingMessages(String userId, 
-			Calendar timestamp, int maxMessages);
+	//Post a reMessage (RT)
+	public boolean postReMessage(String userId, Long messageId);
 	
-	boolean deleteMessage(String userId, String messageId);
+	//Get all messages from a user
+	public List<Message> getUserMessages(String userId);
 	
+	//Get maxMessages from the people followed by userId
+	//posted before time
+	public List<Message> getFollowingMessages(String userId, 
+			Calendar time, int maxMessages);
+	
+	//Delete the message identified by messageId
+	public boolean deleteMessage(Long messageId);
+	
+	//Get the content of a message
+	public String getMessage();
+	
+	//Get the author of a message
+	public String getUser();
+	
+	//Get the date of a message
+	public Calendar getDate();
+	
+	//Get the place from where the message was posted
+	//Returns null if there is no place information
+	public String getPlace();
+	
+	//Get the imageId of a message
+	//Returns null if there is no image associated
+	public Long getImageId();
 	
 }
--- Quack/src/quack/MessageImpl.java
+++ Quack/src/quack/MessageImpl.java
@@ -1,11 +1,13 @@
 package quack;
 
 import java.util.Calendar;
+import java.util.List;
 
 //@Entity
-public class MessageImpl {
+public class MessageImpl implements Message {
 	//@Id
 	//@GeneratedValue
+	/*
 	private Long id;
 	
     private	Calendar timestamp;
@@ -13,5 +15,74 @@ public class MessageImpl {
 	private Long user;
 	private String image;
 	private Long parent;
+	*/
+	
+	@Override
+	public boolean postMessage(String userId, String message) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	@Override
+	public boolean postMessage(String userId, String message, String place) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	@Override
+	public boolean postMessage(String userId, String message, Long imageId) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	@Override
+	public boolean postMessage(String userId, String message, String place,
+			Long imageId) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	@Override
+	public boolean postReMessage(String userId, Long messageId) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	@Override
+	public List<Message> getUserMessages(String userId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	@Override
+	public List<Message> getFollowingMessages(String userId, Calendar time,
+			int maxMessages) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	@Override
+	public boolean deleteMessage(Long messageId) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	@Override
+	public String getMessage() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	@Override
+	public String getUser() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	@Override
+	public Calendar getDate() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	@Override
+	public String getPlace() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	@Override
+	public Long getImageId() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 	
 }
