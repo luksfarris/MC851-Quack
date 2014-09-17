6b1d89d0544d02df2ab2df007ddd8c44425b11e5
augustomorgan <augustosantosmorgan@gmail.com>
2014-09-17 19:55:37 -0300

    interface de mensagem básica

--- Quack/src/quack/Message.java
+++ Quack/src/quack/Message.java
@@ -1,5 +1,24 @@
 package quack;
 
-public interface Message {
+import java.util.Calendar;
+import java.util.List;
 
+
+
+public interface Message {
+	
+	boolean postMessage(String userId, String message);
+	
+	//boolean postMessage(String userId, String message, String imageLink);
+	
+	boolean postReMessage(String userId, String messageId);
+	
+	List<MessageImpl> getUserMessages(String userId);
+	
+	List<MessageImpl> getFollowingMessages(String userId, 
+			Calendar timestamp, int maxMessages);
+	
+	boolean deleteMessage(String userId, String messageId);
+	
+	
 }
