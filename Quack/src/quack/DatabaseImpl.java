632d556c9c69b754cb6000047f20e4e8df8d7c7a
Arthur Eduardo Skaetta Alvarez <ra116180@faith.lab.ic.unicamp.br>
2014-09-23 20:12:00 -0300

    Criacao interface banco de dados

---
+++ Quack/src/quack/DatabaseImpl.java
@@ -0,0 +1,32 @@
+package quack;
+
+import java.util.List;
+
+public class DatabaseImpl implements Database {
+
+	@Override
+	public boolean connect() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean closeConnection() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public List<Object> runQuery(String query) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean runCommand(String command) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	
+}
