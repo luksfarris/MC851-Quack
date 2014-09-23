f77687f6cdac7e3bf5b001f73eecfe7753293c4f
augustomorgan <augustosantosmorgan@gmail.com>
2014-09-23 19:42:11 -0300

    mudança nas interfaces e alguns possíveis construtores

--- Quack/src/quack/Message.java
+++ Quack/src/quack/Message.java
@@ -4,39 +4,15 @@ import java.util.Calendar;
 import java.util.List;
 
 public interface Message {
-	
-	//Insere uma mensagem com conteudo {message} do usuário {user}
+	//Salva a mensagem no banco de dados
 	//Retorna sucesso ou falha na inserção
-	public boolean postMessage(User user, String message);
+	public boolean save();
 	
-	//Insere uma mensagem com conteudo {message} do usuário {user} e local {place}
-	//Retorna sucesso ou falha na inserção
-	public boolean postMessage(User user, String message, String place);
-	
-	//Insere uma mensagem que é uma repostagem de outra mensagem identificada por {messageId} 
-	//do usuário {user}
-	//Retorna sucesso ou falha na inserção
-	public boolean postReMessage(User user, Long messageId);
-	
-	//Retorna a lista de mensagens do usuario {user} com no máximo {maxMessages}
-	public List<Message> getUserMessages(User user, int maxMessages);
-	
-	//Retorna a lista das ultimas mensagens ordenadas por data de postagem com no máximo {maxMessages},
-	//postadas pelas pessoas que o usuário {user} segue.
-	public List<Message> getFollowingMessages(String userId, 
-				int maxMessages);
-	
-	//Retorna a lista de mensagens ordenadas por data de postagem com no máximo {maxMessages},
-	//postadas pelas pessoas que o usuário {user} segue, até a data indicada{time} 
-	public List<Message> getMoreFollowingMessages(String userId, 
-			Calendar time, int maxMessages);
-	
-	
-	//Deleta a mensagem identificada por {messageId} do banco
-	public boolean deleteMessage(Long messageId);
+	//Deleta a mensagem do banco
+	public boolean deleteMessage();
 	
 	//Retorna o conteúdo do atributo de texto da mensagem
-	public String getMessage();
+	public String getText();
 	
 	//Retorna o autor da mensagem
 	public User getUser();
--- Quack/src/quack/MessageImpl.java
+++ Quack/src/quack/MessageImpl.java
@@ -1,87 +1,100 @@
 package quack;
 
 import java.util.Calendar;
-import java.util.List;
 
 //@Entity
 public class MessageImpl implements Message {
 	//@Id
 	//@GeneratedValue
-	/*
+	
 	private Long id;
 		
 	private	Calendar timestamp;
 	private String body;
-	private Long user;
-	private String image;
+	private User user;
+	private String place;
 	private Long parent;
-	*/
+	
+	//Diferentes construtores para a classe, com diferentes parâmetros
+	public MessageImpl(Calendar timestamp, String body, 
+			User user){
+		//TODO mudar o id
+		this.id = new Long(1);
+		this.timestamp = timestamp;
+		this.body = body;
+		this.user = user;
+		this.place = null;
+		this.parent = null;
+	}
+	
+	public MessageImpl(Calendar timestamp, String body, 
+			User user, String place){
+		//TODO mudar o id
+		this.id = new Long(1);
+		this.timestamp = timestamp;
+		this.body = body;
+		this.user = user;
+		this.place = place;
+		this.parent = null;
+	}
+	
+	public MessageImpl(Calendar timestamp, String body, 
+			User user, Long parent){
+		//TODO mudar o id
+		this.id = new Long(1);
+		this.timestamp = timestamp;
+		//TODO mudar o corpo da mensagem pro corpo da mensagem pai
+		this.body = null;
+		this.user = user;
+		this.place = null;
+		this.parent = parent;
+	}
+	
+	public MessageImpl(Calendar timestamp, String body, 
+			User user, String place, Long parent){
+		//TODO mudar o id
+		this.id = new Long(1);
+		this.timestamp = timestamp;
+		//TODO mudar o corpo da mensagem pro corpo da mensagem pai
+		this.body = null;
+		this.user = user;
+		this.place = place;
+		this.parent = parent;
+	}
 	
 	@Override
-	public boolean postMessage(User user, String message) {
+	public boolean save() {
 		// TODO Auto-generated method stub
 		return false;
 	}
 
 	@Override
-	public boolean postMessage(User user, String message, String place) {
+	public boolean deleteMessage() {
 		// TODO Auto-generated method stub
 		return false;
 	}
 
 	@Override
-	public boolean postReMessage(User user, Long messageId) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public List<Message> getUserMessages(User user, int maxMessages) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public List<Message> getFollowingMessages(String userId, int maxMessages) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public List<Message> getMoreFollowingMessages(String userId, Calendar time,
-			int maxMessages) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public boolean deleteMessage(Long messageId) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public String getMessage() {
-		// TODO Auto-generated method stub
-		return null;
+	public String getText() {
+		if(parent == null)
+			return body;
+		//TODO- mudar o caso de ter parent
+		else return null;
 	}
 
 	@Override
 	public User getUser() {
-		// TODO Auto-generated method stub
-		return null;
+		return user;
 	}
 
 	@Override
 	public Calendar getDate() {
-		// TODO Auto-generated method stub
-		return null;
+		return timestamp;
 	}
 
 	@Override
 	public String getPlace() {
-		// TODO Auto-generated method stub
-		return null;
+		return place;
 	}
 	
 	
