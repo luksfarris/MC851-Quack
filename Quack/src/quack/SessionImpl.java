a7add9ee69fec7c8cda83a6bfa313af7beb0a499
Lucas Farris <lucas@farris.com.br>
2014-09-23 19:47:37 -0300

    Adicionando configuracoes

--- Quack/src/quack/SessionImpl.java
+++ Quack/src/quack/SessionImpl.java
@@ -2,18 +2,33 @@ package quack;
 
 public class SessionImpl implements Session {
 
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see quack.Session#createNewSession(java.lang.String)
+	 */
 	@Override
 	public double createNewSession(String userId) {
 		// TODO: gerenciar sessão.
 		return 0;
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see quack.Session#isUserSessionActive(java.lang.String)
+	 */
 	@Override
 	public boolean isUserSessionActive(String userId) {
 		// TODO: checar se usuario esta ativo.
 		return false;
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see quack.Session#terminateUserSession(java.lang.String)
+	 */
 	@Override
 	public void terminateUserSession(String userId) {
 		// TODO: encerra a sessão do usuário.
