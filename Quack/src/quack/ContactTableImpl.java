943c698d73a669d22259a0bf8f7e09831853577d
Lucas Farris <lucas@farris.com.br>
2014-09-24 20:01:10 -0300

    Adicionando classes ContactTable e ContactTableImpl

---
+++ Quack/src/quack/ContactTableImpl.java
@@ -0,0 +1,11 @@
+package quack;
+
+public class ContactTableImpl implements ContactTable {
+
+	@Override
+	public void initialize(Database database) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
