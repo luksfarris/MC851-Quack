59690f8016d51375d363c86464a2078f6e96ad17
augustomorgan <augustosantosmorgan@gmail.com>
2014-09-30 19:25:10 -0300

    Adequando comentários da interface Message

--- Quack/src/quack/Message.java
+++ Quack/src/quack/Message.java
@@ -4,22 +4,31 @@ import java.util.Calendar;
 
 public interface Message {
 
-	//Deleta a mensagem do banco
-	public boolean delete();
-	
-	//Retorna o id no banco da mensagem
 	public Long getId();
+	//Retorna o id no banco da mensagem de {this}
 		
-	//Retorna o conteúdo do atributo de texto da mensagem
+	
 	public String getText();
+	//Retorna o conteúdo do atributo de texto da mensagem de {this}
 	
-	//Retorna o autor da mensagem
+	
 	public User getUser();
+	//Retorna o autor da mensagem {this}
 	
-	//Retorna a data de publicação da mensagem
+	
 	public Calendar getDate();
+	//Retorna a data de publicação da mensagem {this}
 	
-	//Retorna o local de publicação da mensagem, ou null se essa informação
-	//não existe
+	
 	public String getPlace();
+	//Retorna o local de publicação da mensagem {this}, ou null se essa informação
+	//não existe
+	
+	
+	public boolean delete();
+	//Deleta a mensagem {this} da base de dados
+	//Esse método é normalmente chamado quando o usuário deseja deletar
+	//uma mensagem sua. Devemos pensar como garantir a autenticação para
+	//evitar que um invasor delete as mensagens de outras pessoas.
+	
 }
