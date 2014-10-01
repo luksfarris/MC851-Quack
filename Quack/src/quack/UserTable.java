0f4ff52e64a9d28f59b3782eea54a66ac55e8a6f
Jorge Stolfi <stolfi@ic.unicamp.br>
2014-10-01 20:34:21 -0300

    Update UserTable.java
    *Melhorando comentários
    *Retirado método "delete" (pois contas fechadas devem ser marcadas, não removidas)
    *Retirado método duplicado "fromLoginName" (já havia "getUserByLogin")
    *Nomes de argumentos padronizados: Name --> loginName
    *Método getUsersByName --> listUsersByFullName

--- Quack/src/quack/UserTable.java
+++ Quack/src/quack/UserTable.java
@@ -3,27 +3,43 @@ package quack;
 import java.util.*;
 
 public interface UserTable {
+	
+	// Uma instância desta classe é uma tabela de {User}s que permite
+	// atualizações e buscas iversas.
+	//
+	// Enquanto o processo {Server} estiver rodando, deve existir apenas 
+	// uma instância desta classe, que é a lista de todos os usuários 
+	// da rede {Quack}.  Quando o {Server} começa a rodar, ele cria 
+	// uma {UserTable} carregando na memória todo o conteúdo
+	// da base de dados persistente (veja {inicialize} abaixo).
+	// Quando um usuário é criado, ele atualiza esta tabela e também 
+	// grava o usuário na base de dados persistente.
+	
+	// ---------------------------------------------------------------------
+	// INICIALIZAÇÃO E ADIÇÃO DE USUÁRIOS
 
 	public void initialize(Database db);
 	// Inicia uma instância de {this} e carrega todos os usuários
 	// do banco {db} para uma lista de elementos {User}.
-	
-	public User getUserByLoginName(String loginName);
-	// Retorna o {User} de {this} cujo nome de usuário é {username}
-	// ou null se não existir.
-	
-	public List<User> getUsersByFullName(String fullName);
-	// Retorna uma lista de {User} com todos os usuários cujo nome
-	// contém {name}, ou uma lista vazia se não houver.
-	
-	
+
 	public void add(User user);
-	// Adiciona {user} à lista de {this}.
+	// Adiciona {user} à tabela {this}.  Deve ser chamado sempre que um novo
+	// usuário é criado.
 	
-	public void remove(User user);
-	// Remove {user} da lista de {this}.
+	// ------------------------------------------------------------------------------
+	// BUSCA DE USUÁRIO ESPECÍFICO
+
+	public User getUserByLoginName(String loginName);
+	// Retorna o {User} na tabela {this} que tem o nome de login {loginName}, ou {null} se não existir.
 
 	public User getUserByEmail(String email);
-	// Pega do banco de dados um objeto {User} que tem o email {email}
+	// Retorna o {User} na tabela {this} que tem o email {email}, ou {null} se não existir.
+	
+	// BUSCA DE USUÁRIOS POR OUTROS CRITÉRIOS
+	
+	public List<User> listUsersByFullName(String name);
+	// Retorna uma lista de {User} com todos os usuários cujo nome completo
+	// contém {name}, ou uma lista vazia se não houver.
+	
 	
 }
