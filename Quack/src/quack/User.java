5e9ad7a803a24a34f9bccc29ba5c6b1dfe4ff882
Jorge Stolfi <stolfi@ic.unicamp.br>
2014-10-01 21:37:42 -0300

    Acertos de nomes de métodos, comentários
    Removidos os métodos {following} e {followers}, são {getDirectContacts} e {getReverseContacts} + filtro.
    Eliminado getFollowingMessages(start,end), não dá para implementar no {UserImpl}, tem que ser no {ServerImpl}
    Mudados nomes de métodos: 
    getAllMessages() --> getPostedMessages()
    getMessages(start,end) --> getPostedMessages(startTime,endTime,maxN)

--- Quack/src/quack/User.java
+++ Quack/src/quack/User.java
@@ -12,11 +12,11 @@ public interface User {
 	public boolean initialize(String loginName, String email, String fullName,
 			String password, long dbIndex);
 	// Inicializa um objeto {User} recém-criado com os parametros passados. Retorna <true> se
-	// houve sucesso, ou <false> caso ocorra algum erro.  Atribui ao mesmo o número
-	// {dbIndex} para fins de identificação no banco de dados.
+	// houve sucesso, ou <false> caso ocorra algum erro.  O parâmetro {dbIndex} 
+	// vai identificar o usuário no banco de dados persistente.
 	
-	public long getDbIndex();
-	// Retorna o índice de {this}.
+	// -------------------------------------------------------------------------------
+	// ATRIBUTOS BÁSICOS
 	
 	public String getLoginName();
 	// Retorna o nome de usuário de {this}.
@@ -25,62 +25,67 @@ public interface User {
 	// Retorna o nome de {this}.
 	
 	public String getEmail();
-	// Retorna o e-mail de {this}..
-	
-	public Calendar getCreatedDate();
-	// Retorna o timestamp da data em que o perfil de {this} foi criado.
-
-	public boolean follow(User followed);
-	// Faz com que {followed} passe a ser seguido por {this}.
-	// Retorna se a operação foi bem sucedida.
-	
-	public boolean unfollow(User followed);
-	// Faz com que {followed} deixe de ser seguido por {this}.
-	// Retorna se a operação foi bem sucedida.
-
-	public boolean changePassword(String newPassword);
-	// Altera a senha de {this} para {newPassword}.
-	// Retorna se a operação foi bem sucedida.
+	// Retorna o e-mail de {this}.
 	
 	public boolean changeEmail(String newEmail);
 	// Altera o e-mail de {this} para {newEmail}.
-	// Retorna se a operação foi bem sucedida.
-	
-	public boolean block(User user);
-	// Faz com que {user} seja bloqueado por {this}.
-	// Retorna se a operação foi bem sucedida.
-	
-	public boolean unblock(User user);
-	// Faz com que {user} deixe de ser bloqueado por {this}.
-	// Retorna se a operação foi bem sucedida.
+	// Retorna {true} se a operação foi bem sucedida.
 
-	public List<User> following();
-	// Retorna uma lista de todos os usuários que {this} segue.
-	
-	public List<User> followers();
-	// Retorna uma lista de todos os usuários que seguem {this}.
-
-	public List<Message> getMessages(int start, int end);
-	// Retorna uma lista de mensagens em ordem cronológica
-	// decrescente postadas por {this} cujo índice
-	// na consulta está no intervalo [{start}, {end}].
-	
-	public List<Message> getFollowingMessages(int start, int end);
-	// Retorna uma lista de mensagens em ordem cronológica 
-	// decrescente postadas ou repostadas por usuários seguidos por
-	// {this} ou que mencionam {this} cujo índice na consulta
-	// está no intervalo [{start}, {end}].
+	public boolean changePassword(String newPassword);
+	// Altera a senha de {this} para {newPassword}.
+	// Retorna {true} se a operação foi bem sucedida.
 	
 	public boolean checkPassword(String password);
 	// Retorna se a senha de {this} é {password}.
+	
+	public Calendar getCreateTime();
+	// Retorna o timestamp da data em que o usuário {this} foi acrescentado à rede {Quack}.
+
+	public long getDbIndex();
+	// Retorna o índice do usuário {this} na base de dados persistente.
+	
+	// -------------------------------------------------------------------------------
+	// CONTATOS
+	//
+	// Um contato é um par ordenado de usuários, com alguns atributos, {(A,B,...)} representado por uma
+	// instância da classe {Contact}.  Pode indicar que {A} segue {B}, {A} bloqueia {B}, etc.
 
 	public List<Contact> getDirectContacts();
-	// Retorna a lista de quem {this} segue/bloqueou.
+	// Retorna a lista de todos os contatos que tem como origem o usuário {this};
+	// isto é, quem ele segue/bloqueou.
 	
 	public List<Contact> getReverseContacts();
-	// Retorna a lista de quem segue/bloqueou {this}.
+	// Retorna a lista de todos os contatos que tem como destino o usuário {this};
+	// isto é, quem o segue/bloqueou {this}.
+
+	public boolean follow(User target);
+	// Faz com que o usuário {target} passe a ser seguido por {this}.
+	// Retorna {true} se a operação foi bem sucedida.
 	
-	public List<Message> getAllMessages();
-	// Retorna a lista de mensagens de {this}.
+	public boolean unfollow(User target);
+	// Faz com que o usuário {target} deixe de ser seguido por {this}.
+	// Retorna {true} se a operação foi bem sucedida.
+	
+	public boolean block(User target);
+	// Faz com que o usuário {target} seja bloqueado por {this}.
+	// Retorna {true} se a operação foi bem sucedida.
+	
+	public boolean unblock(User target);
+	// Faz com que o usuário {target} deixe de ser bloqueado por {this}.
+	// Retorna {true} se a operação foi bem sucedida.
+	
+	// -------------------------------------------------------------------------------
+	// MENSAGENS
+	
+	// Cada usuário tem uma lista de mensagens que ele postou (incluindo re-postagens)
+	
+	public List<Message> getPostedMessages();
+	// Retorna a lista de mensagens postadas pelo usuário {this}, em ordem cronológica
+	// inversa (mais recente primeiro).
+	
+	public List<Message> getPostedMessages(Calendar startTime, Calendar endTime, long maxN);
+	// Retorna o trecho da lista de mensagens {this.getMessages()},
+	// limitadas ao intervalo definido por {startTime,endTime,maxN}. 
+	// Veja a interpretação destes parâmetros na interface {Server}.
 	
 }
