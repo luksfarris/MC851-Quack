a26727f97977cdb4769463f5f769fb9250f6bd8a
Jorge Stolfi <stolfi@ic.unicamp.br>
2014-10-01 22:47:59 -0300

    Mudança na lógica de contatos
    Para atualzar a base de dados persistente, segundo o plano atual, é necessário que a lógica de acréscimo e alteração de contatos seja feita na classe {ServerImpl}, e não em {UserImpl}.  Assim, foi necessário trocar os métodos {follow,block,unfollow,unblock} por métodos que simplesmente procuram um {Contact} na lista de contatos e acrescentam um novo {Contact}, supondo que ainda não existe.
    Veja {getDirectContact,getReverseContact,addDirectContact,addReverseContact}

--- Quack/src/quack/User.java
+++ Quack/src/quack/User.java
@@ -52,32 +52,46 @@ public interface User {
 	// -------------------------------------------------------------------------------
 	// CONTATOS
 	//
-	// Um contato é um par ordenado de usuários, com alguns atributos, {(A,B,...)} representado por uma
-	// instância da classe {Contact}.  Pode indicar que {A} segue {B}, {A} bloqueia {B}, etc.
+	// Um contato é um par ordenado de usuários com mais alguns atributos, {(A,B,...)},
+	// isto é, uma aresta do grafo da rede {Quack}.  É representado por uma
+	// instância da classe {Contact}.  Pode indicar que o /usuário de origem/ {A} 
+	// segue (ou bloqueia, ou tem alguma outra relação) com o /usuário alvo/ {B}.
+	//
+	// Há no máximo um contato, ativo ou inativo, para cada par de usuários (origem e alvo).
+	// Por enquanto, contatos nunca são eliminados, apenas podem ter seus atributos
+	// alterados de modo a se tornarem inativos.  
+	//
+	// Cada instância {this} de {User} tem uma lista de /contatos diretos/, em 
+	// que o usuário {this} é origem; isto é, os usuários que ele segue/bloqueou/etc..
+	// Tem também uma lista de /contatos reversos/, em que ele é destino;
+	// isto é, quem segue/bloqueou o usuário {this}. Sempre que um contato 
+	// {(A,B,...)} é acrescentado á lista de contatos diretos de {A},
+	// deve ser acrescentado também à lista de contatos reversos de {B},
+	//pata manter a consistência da rede.
 
 	public List<Contact> getDirectContacts();
-	// Retorna a lista de todos os contatos que tem como origem o usuário {this};
-	// isto é, quem ele segue/bloqueou.
+	// Retorna a lista de todos os contatos que tem como origem o usuário {this}.
 	
 	public List<Contact> getReverseContacts();
-	// Retorna a lista de todos os contatos que tem como destino o usuário {this};
-	// isto é, quem o segue/bloqueou {this}.
+	// Retorna a lista de todos os contatos que tem como destino o usuário {this}.
+	
+	public Contact getDirectContact(User target);
+	// Retorna o contato da lista {this.getDirectContacts()} que tem
+	// como alvo o usuário {target}; ou {null} se não existir.
+	
+	public Contact getReverseContact(User origin);
+	// Retorna o contato da lista {this.getReverseContacts()} que tem
+	// como origem o usuário {origin}; ou {null} se não existir.
 
-	public boolean follow(User target);
-	// Faz com que o usuário {target} passe a ser seguido por {this}.
-	// Retorna {true} se a operação foi bem sucedida.
+	public void addDirectContact(Contact contact);
+	// Acrescenta à lista {this.getDirectContacts()} o contato dado, que deve ter.
+	// o usuário {this} como origem.  Aborta, caso já exista nessa lista 
+	// algum contato com o mesmo alo que o alvo de {contact}.
 	
-	public boolean unfollow(User target);
-	// Faz com que o usuário {target} deixe de ser seguido por {this}.
-	// Retorna {true} se a operação foi bem sucedida.
-	
-	public boolean block(User target);
-	// Faz com que o usuário {target} seja bloqueado por {this}.
-	// Retorna {true} se a operação foi bem sucedida.
-	
-	public boolean unblock(User target);
-	// Faz com que o usuário {target} deixe de ser bloqueado por {this}.
-	// Retorna {true} se a operação foi bem sucedida.
+	public void addReverseContact(Contact contact);
+	// Acrescenta à lista {this.getDirectContacts()} o contato dado, que deve ter.
+	// o usuário {this} como alvo.  Aborta, se já houver nessa lista 
+	// algum contato com o mesmo usuário origem que a origem de {contact}.
 	
 	// -------------------------------------------------------------------------------
 	// MENSAGENS
