c55342af0466d71041a3b6a94d957e5cab72486c
Lucas Farris <lucas@farris.com.br>
2014-10-07 19:22:06 -0300

    Tornando classe htmlimpl compilável

--- Quack/src/quack/HTMLImpl.java
+++ Quack/src/quack/HTMLImpl.java
@@ -17,25 +17,32 @@ public class HTMLImpl implements HTML {
 	}
 
 	@Override
-	public HTML initialize() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
 	public String homePage() {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
+
 	@Override
-	public String loginSuccessfulPage(String cookie) {
+	public HTML initialize(Server server) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
-	public String messageListPage(String cookie, String userName,
+	public String loginSuccessfulPage(String cookie, User user) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String userProfilePage(String cookie, User source, User target) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String messageListPage(String cookie, String title, User user,
 			List<Message> messages, int maxN) {
 		// TODO Auto-generated method stub
 		return null;
