1f9f5abe1d47505a634ad4724091062c3b73819a
Arthur Eduardo Skaetta Alvarez <ra116180@legiao.lab.ic.unicamp.br>
2014-10-07 19:28:54 -0300

    UserTableImpl adequado a nova versao

--- Quack/src/quack/UserTableImpl.java
+++ Quack/src/quack/UserTableImpl.java
@@ -5,52 +5,41 @@ import java.util.*;
 public class UserTableImpl implements UserTable {
 
 	private List<User> table;
-	
+
 	@Override
 	public void initialize(Database db) {
-		// TODO Auto-generated method stub
 		
-	}
-
-	@Override
-	public User getUserByLoginName(String loginName) {
-		
-		for(User u: table){
-			if(u.getLoginName() == loginName)
-				return u;
-		}
-		
-		return null;
-	}
-
-	@Override
-	public List<User> getUsersByFullName(String fullName) {
-		List<User> l = new LinkedList<User>();
-		
-		for(User u: table){
-			if(u.getFullName() == fullName)
-				l.add(u);
-		}
-		return l;
 	}
 
 	@Override
 	public void add(User user) {
-		// TODO Auto-generated method stub
-		table.add(user);
-	}
-
-	@Override
-	public void remove(User user) {
-		table.remove(user);
 		
+		this.table.add(user);
 	}
 
-
 	@Override
-	public User getUserByEmail(String email) {
-		// TODO Auto-generated method stub
+	public User getUserByLoginName(String loginName) {
+		for(User u : this.table){
+			if(u.getLoginName() == loginName)
+				return u;
+		}
 		return null;
 	}
 
+	@Override
+	public User getUserByEmail(String email) {
+		for(User u : this.table){
+			if(u.getEmail() == email)
+				return u;
+		}		return null;
+	}
+
+	@Override
+	public List<User> listUsersByFullName(String name) {
+		for(User u : this.table){
+			if(u.getFullName() == name)
+				return u;
+		}		
+		return null;
+	}
 }
