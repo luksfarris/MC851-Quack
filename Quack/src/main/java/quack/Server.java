9ecad5f7a4e80908b52208df45544ef6deaa8251
Jorge Stolfi <stolfi@ic.unicamp.br>
2014-10-29 20:43:42 -0200

    Update Server.java

--- Quack/src/main/java/quack/Server.java
+++ Quack/src/main/java/quack/Server.java
@@ -45,30 +45,24 @@ public interface Server {
 	// datahora é uma {String} no formato "%Y-%m-%d %H:%M:%S (%Z)" onde
 	// "%Z" é o fuso horário, por exemplo "(+0300)" para UTC + 3 horas.
 	// As datahoras neste formato externo serão convertidas
-	// internamente para um valor de tipo {Calendar}: um número inteiro
-	// de segundos na zona padrão UTC. ??{ Confirmar se {Calendar} é isso mesmo.
-	// }??
+	// internamente para um {long int}: um número inteiro
+	// de segundos na zona padrão UTC.
 	//
 	// INTERVALOS DE MENSAGENS
 	//
-	// Alguns dos métodos a seguir especificam um trecho de uma lista de
-	// mensagens através
-	// de parâmetros {startTime}, {endTime} (datahoras), e {maxN} (inteiro
-	// positivo).
+	// Alguns dos métodos a seguir (e em outras interfaces) especificam um
+	// trecho de uma lista de mensagens através de parâmetros {startTime},
+	// {endTime} (datahoras), e {maxN} (inteiro positivo).
 	//
 	// Supõe-se que a lista completa de mensagens em questão está ordenada em
 	// ordem *decrescente* de datahora (de envio ou re-envio); isto é,
 	// mensagens mais recentes no início. O trecho selecionado da lista consiste
-	// das
-	// mensagens com datahora maior ou igual a {startTime} e menor ou igual a
-	// {endTime},
-	// truncadas em no máximo {|maxN|} mensagens.
+	// das mensagens com datahora maior ou igual a {startTime} e menor ou igual a
+	// {endTime}, truncadas em no máximo {|maxN|} mensagens.
 	//
 	// Se {endTime} for omitido ("" ou {null} no formato externo, -1 no formato
-	// interno),
-	// supõe-se que {endTime} é o fim dos tempos. Se {startTime} for omitido
-	// (idem), supõe-se que
-	// {startTime} é o início dos tempos.
+	// interno), supõe-se que {endTime} é o fim dos tempos. Se {startTime} for 
+	// omitido (idem), supõe-se que {startTime} é o início dos tempos.
 	//
 	// Se houver mais do que {|maxN|} mensagens nesse intervalo de datahoras,
 	// o trecho desejado são apenas as {|maxN|} mensagens mais antigas se {maxN}
@@ -105,61 +99,66 @@ public interface Server {
 	// Os métodos abaixo são ativados pelo servidor HTTP ao receber
 	// certos comandos GET ou POST do browser do usuário.
 	//
-	// ??{ Descrever aqui detalhes sobre o processo de conversão do
-	// GET/PUT em chamada de métodos, incluindo obtenção dos parâmetros. }??
+	// Cada método tem parâmetros {request}, {response}, e {context}.
+	// O parâmetro {request} é basicamente uma tabelas de pares
+	// {chave,valor}, extraídos do commando GET ou POST.
 	//
-	// O resultado da chamada de cada um destes métodos é uma
-	// página HTML que deve ser enviada ao browser do usuário.
+	// O parâmetro {response} é um objeto onde cada método deve 
+	// escrever o resultado do GET ou POST, uma página HTML que 
+	// é passada ao servidor HTTP e devolvida por este ao usuário.
 	// Em caso de erro, geralmente a página contém uma mensagem
 	// explicativa.
 	//
-	// ??{ Descrever a "saída" de cada método, ou seja a
-	// página HTML que é resposta ao GET/POST. }??
+	// ??{ Descrever o parâmetro {context} e quando {IOException} é levantada. }??
 
 	public String processHomePageReq();
 
 	// Chamado quando o servidor HTTP recebe um pedido de vista da home page da
-	// rede {Quack}.
+	// rede {Quack}. 
+	//
 	// Nessa página haverá botões/campos para fazer login e cadastrar novo
-	// usuário, listar as mensagens
-	// públicas ou o perfil de qualquer usuário, busca de mensagens
-	// e usuários pelo conteúdo, etc.
+	// usuário, listar as mensagens públicas ou o perfil de qualquer usuário, 
+	// busca de mensagens e usuários pelo conteúdo, etc.
 
 	public void processRegistrationReq(HttpServletRequest request,
 			HttpServletResponse response, ServletContext context) throws IOException;
 
 	// Chamado quando o servidor HTTP recebe um pedido de cadastramento de novo
-	// usuário no {Quack},
-	// Cria um novo usuário com dados loginName, email, fullName e
-	// password, parametros provenientes da variavel {request}.
+	// usuário no {Quack}.
+	// 
+	// Cria um novo usuário com dados {loginName}, {email}, {fullName} e
+	// {password}, que devem estar especificados no parâmetro {request}.
 	// Acrescenta-o ao cadastro {this.userTable} (e à base de dados
 	// persistente).
 	//
 	// Por enquanto, o cadastramento não faz o login automático. Se o
-	// cadastramento tiver
-	// sucesso, devolve uma página em {response} que mostra o perfil público do usuário.
+	// cadastramento tiver sucesso, devolve uma página em {response} 
+	// que mostra o perfil público do usuário.
 
 	public void processLoginReq(HttpServletRequest request,
 			HttpServletResponse response, ServletContext context) throws IOException;
 
 	// Chamado quando o servidor HTTP recebe um pedido de login de um usuário
-	// existente
-	// da rede {Quack}. O parâmetros parâmetro {request} contem os
-	// campos login e password que o usuario preencheu na pagina
-	// de login.
+	// existente da rede {Quack}. 
+	//
+	// O parâmetro {request} deve conter os campos {login} e {password} 
+	// que o usuario preencheu na pagina de login.
 	//
 	// Se o login tiver sucesso, a sessão é criada e acrescentada ao
-	// table de sessões abertas. O resultado é ser redirecionado
-	// à pagina de perfil do usuario atraves de {response}
+	// table de sessões abertas. O resultado, devolvido em 
+	// {response}, é uma página mostrando o perfil privado do usuario
 
 
 	public void processLogoutReq(HttpServletRequest request,
 			HttpServletResponse response, ServletContext context) throws IOException;
 
 	// Chamado quando o servidor HTTP recebe um pedido de logout do usuário.
-	// A sessão de request é então fechada pelo servidor se ele existir no sistema.
+	// 
+	// O parâmetro {request} deve conter um campo {cookie} que identifica a sessão.
+	// Essa sessão será fechada.  Falha se o {cookie} não corresponder a uma sessão 
+	// atualmente aberta.
 	//
-	// Em caso de sucesso, o respose é a homepage do sistema {Quack},
+	// Em caso de sucesso, o parâmetro {respose} conterá a homepage do sistema {Quack},
 	// como vista por usuários não logados.
 
 	public String processShowUserProfileReq(String cookie, String loginName);
