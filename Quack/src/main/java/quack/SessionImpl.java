277d06baa05cfe008a0547d575d78bc7d2085a3f
Lucas Farris <lucas@farris.com.br>
2014-11-12 22:37:22 -0200

    Removendo implementação desnecessária em SessionImpl

--- Quack/src/main/java/quack/SessionImpl.java
+++ Quack/src/main/java/quack/SessionImpl.java
@@ -1,19 +1,13 @@
 package quack;
 
-import java.util.UUID;
-
 public class SessionImpl implements Session {
-
+	
 	private User loggedUser;
 	private String cookie;
 	
 	public void open(User user, String cookie) {
 		loggedUser = user;
-		if (cookie == null || cookie.length() == 0) {
-			this.cookie =  UUID.randomUUID().toString();
-		} else {
-			this.cookie = cookie;
-		}
+		this.cookie = cookie;
 	}
 
 	public String getCookie() {
