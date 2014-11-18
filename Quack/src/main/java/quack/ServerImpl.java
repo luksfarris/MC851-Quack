d11e8ede3d5d40750f1b150922fd9cfb0cd8ca02
augustomorgan <augustosantosmorgan@gmail.com>
2014-11-18 20:24:50 -0200

    Retiradas mensagens de debug do serv Impl e adicionado link para criar mensagens na pagina de usuario

--- Quack/src/main/java/quack/ServerImpl.java
+++ Quack/src/main/java/quack/ServerImpl.java
@@ -422,13 +422,10 @@ public final class ServerImpl implements Server {
 		
 		List<Message> messages = new LinkedList<Message>(); 
 		
-		System.out.println("Contatos: "+user.getDirectContacts().size());
 		for(Contact c : user.getDirectContacts()){
 			if(c.status().equals("Follow")){
-				System.out.println("Messages: "+c.target().getPostedMessages(startTime, endTime, maxN).size());
 				for(Message m : c.target().getPostedMessages(startTime, endTime, maxN)){
 					messages.add(m);
-					System.out.println(m.getText());
 				}
 			}
 		}
@@ -444,10 +441,6 @@ public final class ServerImpl implements Server {
 				else return 1;
 	        }
 		});
-
-		for(Message m : messages){
-			System.out.println(m.getUser().getLoginName()+":  "+ m.getText());
-		}
 		
 		request.getSession().setAttribute("timelineMessages", messages);
 		response.sendRedirect("/Quack/timeline.jsp");
