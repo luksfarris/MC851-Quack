5359b2a047d0e7d1bc291969fdac9b6a165ab0ce
Jorge Stolfi <stolfi@ic.unicamp.br>
2014-12-03 21:07:27 -0200

    Exige timezone "UTC"
    Alterada implementação de acordo com interface
    
    {DEFAULT_FORMAT} retirado o "z", a timezone por enquanto é fixa " UTC".
    
    {toString} 
      exige que o parâmetro {timezone} seja "UTC"
    
    {fromString} 
      exige que o parâmetro {timestamp} termine com " UTC"
      força {formatter} a supor fuso UTC na conversão

--- Quack/src/main/java/quack/TimestampImpl.java
+++ Quack/src/main/java/quack/TimestampImpl.java
@@ -5,7 +5,7 @@ import java.util.*;
 
 public class TimestampImpl implements Timestamp {
 	
-	public static final String DEFAULT_FORMAT = "yyyy-MM-dd HH:mm:ss z";
+	public static final String DEFAULT_FORMAT = "yyyy-MM-dd HH:mm:ss";
 	
 	@Override
 	public long now() {
@@ -14,31 +14,37 @@ public class TimestampImpl implements Timestamp {
 
 	@Override
 	public String toString(long timestamp, String timezone) {
+		// Por enquanto, o fuso horário deve ser UTC:
+		assert timezone.equal("UTC");
 		DateFormat formatter = new SimpleDateFormat(DEFAULT_FORMAT);
 		formatter.setTimeZone(TimeZone.getTimeZone(timezone));
-		return formatter.format(new Date(timestamp * 1000));
+		return formatter.format(new Date(timestamp * 1000)) + " " + timezone;
 	}
 
 	@Override
 	public long fromString(String timestamp) {
+		// Exige que o fuso horário seja UTC por enquanto:
+		String timezone = "UTC";
+		int tzk = timestamp.indexOf(timezone);
+		assert tzk >= 0;
+		timestamp = timestamp.substring(0,tzk).trim();
 		DateFormat formatter = new SimpleDateFormat(DEFAULT_FORMAT);
-		Date date;
+		formatter.setTimeZone(TimeZone.getTimeZone(timezone));
 		try {
-			date = formatter.parse(timestamp);
+			Date date = formatter.parse(timestamp);
 			return date.getTime() / 1000;
 		} catch (ParseException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
+	   	        return 0L;
 		}
-		return 0L;
 	}
 	
 	@Override
 	public long fromString(String timestamp, String format) {
 		DateFormat formatter = new SimpleDateFormat(format);
-		Date date;
 		try {
-			date = formatter.parse(timestamp);
+			Date date = formatter.parse(timestamp);
 			return date.getTime() / 1000;
 		} catch (ParseException e) {
 			// TODO Auto-generated catch block
